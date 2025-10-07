c9b7cb30e26fa354f9835ed75b2469b076ed95c2
Pimlada Patiphatpatawee <pxcopic@gmail.com>
2025-10-07 16:21:51 +0700

    commit#3
    new structure of code in loanManagement.c fixing business logic

---
+++ loanManagement.c
@@ -0,0 +1,198 @@
+#include <stdio.h>
+#include <string.h>
+
+int choice;
+int nextID = 1;
+char line[10000];
+char password[12] = "thisisadmin";
+
+struct userData{
+    char loanID[100000];
+    char borrowName[100000];
+    char approvalDate[100000];
+    int loanAmount;
+};
+
+int readFile(const char *loanDataFile){
+    FILE *file = fopen(loanDataFile, "r");
+    if (file == NULL) return 0;
+    int count = 0;
+
+    while (fgets(line, sizeof(line), file)) {
+        if (strlen(line) > 1) count++;
+    }
+
+    fclose(file);
+    return count;
+}
+
+void addData(){
+    struct userData user;
+    char confirmStatus;
+    int day, month, year, valid;
+    FILE *file = fopen("loan_data.csv", "a");
+    if(file == NULL) printf("File cannot be opened");
+
+    nextID = readFile("loan_data.csv") + 1;
+    sprintf(user.loanID, "L%03d", nextID);
+    
+    printf("Add data\n");
+
+    printf("Enter borrower name : ");
+    scanf(" %[^\n]", user.borrowName);
+    printf("Please enter loan amount : ");
+    scanf("%d", &user.loanAmount);
+    do {
+        printf("Enter approval date (dd/mm/yyyy): ");
+        scanf(" %10s", user.approvalDate);
+        valid = sscanf(user.approvalDate, "%2d/%2d/%4d", &day, &month, &year);
+        if (valid != 3 || strlen(user.approvalDate) != 10 ||
+            day < 1 || day > 31 || month < 1 || month > 12 || year < 1900) {
+            printf("Invalid format! Please enter again.\n");
+            valid = 0;
+        }
+    } while (!valid);
+    printf("Confirm data? (y/n) : ");
+    scanf(" %c", &confirmStatus);
+
+    if(confirmStatus == 'y'){
+        fprintf(file, "%s,%s,%d,%s\n",
+            user.loanID, user.borrowName, user.loanAmount, user.approvalDate);
+        fclose(file);
+        printf("Data saved %s\n", user.loanID);
+    }else{
+        printf("Data not save.");
+        addData();
+    }
+}
+
+void searchData(){
+    char search[100000];
+    int found = 0, count = 0;
+    FILE *file = fopen("loan_data.csv", "r");
+    printf("Search data: ");
+    scanf("%s", search);
+
+    while(fgets(line, sizeof(line), file) != NULL){
+        if(strstr(line, search)){
+            if(count < 1){
+                printf("+----------+------------+-------------+----------------+\n");
+                printf("| Loan ID  | Borrower   | Loan Amount | Approval Date  |\n");
+                printf("+----------+------------+-------------+----------------+\n");
+            }
+            
+            char *loanID = strtok(line, ",");
+            char *borrowName = strtok(NULL, ",");
+            char *loanAmount = strtok(NULL, ",");
+            char *approvalDate = strtok(NULL, "\n");
+
+            printf("| %-8s | %-10s | %-11s | %-14s |\n", loanID, borrowName, loanAmount, approvalDate);
+            found = 1;   
+            printf("+----------+------------+-------------+----------------+\n");
+        }
+        count++;
+    }
+
+    if(found != 1) printf("Data is not found.\n");
+    fclose(file);
+}
+
+int updateFunc(const int *choiceUpdate){
+    
+}
+
+void updateData(){
+    int countLine = 0;
+    char updateStatus;
+    char validatePass[12];
+    int choiceUpdate;
+    FILE *file = fopen("loan_data.csv", "r");
+    int num;
+
+    printf("Update data\n");
+    while(fgets(line, sizeof(line), file) != NULL){
+        if(countLine < 1){
+            printf("+----------+------------+-------------+----------------+\n");
+            printf("| Loan ID  | Borrower   | Loan Amount | Approval Date  |\n");
+            printf("+----------+------------+-------------+----------------+\n");
+        }
+            
+        char *loanID = strtok(line, ",");
+        char *borrowName = strtok(NULL, ",");
+        char *loanAmount = strtok(NULL, ",");
+        char *approvalDate = strtok(NULL, "\n");
+
+        printf("| %-8s | %-10s | %-11s | %-14s |\n", loanID, borrowName, loanAmount, approvalDate);
+        printf("+----------+------------+-------------+----------------+\n");
+        countLine++;
+    }
+
+    printf("Please enter password for admin to update data : ");
+    scanf("%s", validatePass);
+    if(strcmp(validatePass, password) == 0){
+        do{
+            printf("Welcome, admin! Please select element you want to update.\n");
+            printf("1. Borrower\n");
+            printf("2. Loan Amount\n");
+            printf("3. Approval date\n");
+            printf("4. Quit\n");
+            printf("Select : ");
+            scanf("%d", &choiceUpdate);
+
+            switch(choiceUpdate){
+                case 1:
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+
+                default:
+                    break;
+            }
+        }while(choiceUpdate != 4);
+    }else{
+        printf("You do not have permission to update files.\n");
+    }
+
+}
+
+int main(){
+    do{
+        printf("-------------------------------\n");
+        printf("Select the menu\n");
+        printf("1. Add data\n");
+        printf("2. Search data\n");
+        printf("3. Update data\n");
+        printf("4. Delete data\n");
+        printf("5. End program\n");
+        printf("-------------------------------\n");
+        scanf("%d", &choice);
+
+        switch (choice){
+        case 1:
+        addData();
+            break;
+        
+        case 2:
+        searchData();
+            break;
+
+        case 3:
+        updateData();
+            break;
+
+        // case 4:
+        // deleteData();
+        //     break;
+            
+        default:
+            break;
+        }
+    }while(choice != 5);
+
+
+    return 0;
+}
